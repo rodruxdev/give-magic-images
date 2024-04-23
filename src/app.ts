90652778a7d87eeafe348f975c65c237989b2734
Rodrigo Goitia <rodrigojgq@gmail.com>
2024-04-23 10:52:06 -0300

    Add middlewares to handle errors

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,12 @@ import { usersRouter } from "./routes/users";
 import { localStrategy } from "./utils/auth/localStrategy";
 import { JwtStrategy } from "./utils/auth/jwtStrategy";
 import { authRouter } from "./routes/auth";
+import { imagesRouter } from "./routes/images";
+import {
+  boomHandler,
+  errorHandler,
+  logErrors,
+} from "./middlewares/errorHandler";
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -17,6 +23,10 @@ app.use(corsMiddleware());
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
+
+app.use(logErrors);
+app.use(boomHandler);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,40 @@
+import { Boom } from "@hapi/boom";
+import { NextFunction, Request, Response } from "express";
+
+export function logErrors(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log("logErrors");
+  console.log(err);
+  next(err);
+}
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log("errorHandler");
+  res.status(500).json({
+    message: err?.message,
+    stack: err?.stack,
+  });
+}
+
+export function boomHandler(
+  err: Boom,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err.isBoom) {
+    const { output } = err;
+    res.status(output.statusCode).json(output.payload);
+  } else {
+    next(err);
+  }
+}
